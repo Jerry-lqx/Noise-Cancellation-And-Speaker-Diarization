cc356785f25a670d866f8e9c7ffe5bfdc9131737
Jerry-lqx <81498994+Jerry-lqx@users.noreply.github.com>
2022-04-27 16:01:20 -0700

    Add files via upload

---
+++ MFCC_using_librosa/MFCC_librosa.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+
+import librosa 
+import librosa.display
+import soundfile as sf
+import matplotlib.pyplot as plt
+import numpy as np
+
+#read in audio file
+filename = '../recording/two_person_speech.wav'
+y, sr = librosa.load(filename,sr= None)
+
+#create mfcc
+mfcc = librosa.feature.mfcc(y=y, sr=sr)
+
+#display the mfcc plots
+librosa.display.specshow(mfcc)
+plt.ylabel('MFCC')
+
+plt.colorbar()
+
+#save mfcc plots to result folder
+plt.savefig('./result/mfcc.png')
+
+# Passing through arguments to the Mel filters
+S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128,
+                                    fmax=21500)
+fig, ax = plt.subplots()
+S_dB = librosa.power_to_db(S, ref=np.max)
+
+#display melspectrogram plots
+img = librosa.display.specshow(S_dB, x_axis='time',
+                         y_axis='mel', sr=sr,
+                         fmax=21500, ax=ax)
+fig.colorbar(img, ax=ax, format='%+2.0f dB')
+ax.set(title='Mel-frequency spectrogram')
+
+#save melspectrogram plots to result folder
+plt.savefig('./result/Mel-frequency spectrogram.png')
+
+#perform inverse mfcc
+audio = librosa.feature.inverse.mfcc_to_audio(mfcc,sr=sr)
+
+#save the inversed mfcc audio to result
+sf.write('./result/tow_person_speech_inverse_mfcc.wav', audio, sr)
